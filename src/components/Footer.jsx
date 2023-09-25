57508bb1add754d0ec069e4014aae7f1d687a440
Shresth-291 <shresth.2024csit1159@kiet.edu>
2023-09-25 10:13:53 +0530

    Initial Commit

---
+++ src/components/Footer.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <footer>
+        <div>
+            <h1>TechyStar</h1>
+            <p>@All rights reserved</p>
+        </div>
+        <div>
+            <h5>Follow Us</h5>
+            <div>
+                <a href="/twitter" target='blank'>Twitter</a>
+                <a href="/linkedIn" target='blank'>LinkedIn</a>
+                <a href="/github" target='blank'>Github</a>
+            </div>
+        </div>
+    </footer>
+  )
+}
+
+export default Footer
